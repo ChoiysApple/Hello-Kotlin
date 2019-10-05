32ee4a16ebd072b884b316f4802b66c862c8b271
ChoiysApple <chcs1370@gmail.com>
2019-10-05 23:50:19 +0900

    Add result ImageView

--- DiceRoller/app/src/main/java/com/marchengraffiti/architecture/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/marchengraffiti/architecture/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.marchengraffiti.architecture.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -13,14 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ROLL Button
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
     }
 
     private fun rollDice(){
-
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.dice_result_text)
-        resultText.text = (Random().nextInt(6) + 1).toString()
+
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val resultImage: ImageView = findViewById(R.id.dice_result_text)
+        resultImage.setImageResource(drawableResource)
     }
 }
