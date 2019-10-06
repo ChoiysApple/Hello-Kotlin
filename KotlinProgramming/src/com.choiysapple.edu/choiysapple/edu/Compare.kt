e4c1a0ba2c5bb44df202b8758c87b81ca83e9805
ChoiysApple <chcs1370@gmail.com>
2019-10-06 23:25:06 +0900

    Create Compare.kt

---
+++ KotlinProgramming/src/com.choiysapple.edu/choiysapple/edu/Compare.kt
@@ -0,0 +1,10 @@
+package com.choiysapple.edu.choiysapple.edu
+
+val numberOfFish = 50
+val numberOfPlants = 23
+
+fun main() {
+    if (numberOfFish > numberOfPlants) {
+        print("Good Ratio")
+    }
+}
