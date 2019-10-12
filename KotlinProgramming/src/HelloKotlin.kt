632172b4d01fb68fd961bcf5e5bc7ac8f05defe4
ChoiysApple <chcs1370@gmail.com>
2019-10-12 12:56:47 +0900

    Update  HelloKotlin.kt

--- KotlinProgramming/src/HelloKotlin.kt
+++ KotlinProgramming/src/HelloKotlin.kt
@@ -11,6 +11,13 @@ fun main() {
 
     val isUnit = println("This is an expression")
     println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 }
 
 class Person(val no: Int, val name: String, val jab: String)
